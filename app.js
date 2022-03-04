0b28ebfa9b66a7b0e8a72786d22bb75289cad97b
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-04 21:16:54 +0200

    setup yargs to add, remove, read and list notes

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 const yargs = require("yargs");
 
+// Customize yargs version
+yargs.version("1.1.0");
+
+//Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding new note");
+  },
+});
+
+//Create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function () {
+    console.log("remove a note");
+  },
+});
+
+//Create read command
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function () {
+    console.log("read a note");
+  },
+});
+
+//Create list command
+yargs.command({
+  command: "list",
+  describe: "list notes",
+  handler: function () {
+    console.log("list notes");
+  },
+});
+
 console.log(yargs.argv);
