6a2c3f8361e0eb68a57936703d29734be2401602
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-11 18:36:16 +0200

    adding the ability to serve static files from the puplic folder, html, css and javascript files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,15 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.get('', (req, res) => {
-	res.send('Hello from Express');
-});
-
-app.get('/help', (req, res) => {
-	res.send('Help page');
-});
+app.use(express.static(path.join(process.cwd(), './puplic')));
 
 app.get('/weather', (req, res) => {
-	res.send('Weather page');
-});
-
-app.get('/about', (req, res) => {
-	res.send('About page');
+	res.send({
+		location: 'Cairo',
+		forcast: "It's sunny",
+	});
 });
 
 app.listen(3000, () => {
