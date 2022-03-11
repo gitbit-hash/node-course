387a7e4f5423acdc2107ae4eca5572395c2749b3
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-12 00:48:48 +0200

    adding the ability to get the user input search term and fetch the weather information then show it to the user, styling the input and the button

--- web-server/puplic/js/app.js
+++ web-server/puplic/js/app.js
@@ -1,10 +1,24 @@
-fetch('http://localhost:3000/weather?address=cairo')
-	.then((res) => res.json())
-	.then((data) => {
-		if (data.error) {
-			console.log(data.error);
-		} else {
-			console.log(data.location);
-			console.log(data.forecast);
-		}
-	});
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const location = input.value;
+
+	messageOne.textContent = 'Loading...';
+	messageTwo.textContent = '';
+
+	fetch(`http://localhost:3000/weather?address=${location}`)
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.error) {
+				messageOne.textContent = data.error;
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+			}
+		});
+});
