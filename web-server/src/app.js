d942fc2d21e77b3c52ec552a17df1d41cd21eefe
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-11 19:19:38 +0200

    adding hbs library to handle dynamic content, configure it with express, replace html files with hbs files and add routes for them

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,29 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(process.cwd(), './puplic')));
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather app',
+		name: 'Muhammad Salah',
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About',
+		name: 'Muhammad Salah',
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help Page',
+		message: 'This is the help page',
+	});
+});
 
 app.get('/weather', (req, res) => {
 	res.send({
